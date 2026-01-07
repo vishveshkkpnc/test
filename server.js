e454b9719a83e264459e58f64983feec066a77c6
vishvesh <vishveshsingh739@gmail.com>
2026-01-07 18:58:20 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// Dummy login data (temporary)
+const users = {
+    "admin": "1234",
+    "student01": "vnit123"
+};
+
+// Login route
+app.post("/login", (req, res) => {
+    const { userid, password } = req.body;
+
+    if (users[userid] && users[userid] === password) {
+        res.redirect("/home.html");
+    } else {
+        res.send("<h2>Invalid Login ❌</h2><a href='/'>Go Back</a>");
+    }
+});
+
+// Server
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
